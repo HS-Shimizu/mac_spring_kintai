4fba3183a0c5fc6aadffcf450081a39e63ea4b4e
HS-Shimizu <82071710+HS-Shimizu@users.noreply.github.com>
2022-04-12 09:24:09 +0900

    4/1２始まり時点

--- kintai-bord/src/main/java/com/example/demo/Service/KintaiService.java
+++ kintai-bord/src/main/java/com/example/demo/Service/KintaiService.java
@@ -16,30 +16,67 @@ public class KintaiService {
 
 	@Autowired
 	KintaiRepository kintaiRepository;
-	
+	GregorianCalendar gcalendar = new GregorianCalendar();
+
+	/**
+	 * 現在の年を取得
+	 */
+	public void getYear(DataBean data) {
+
+		//現在の年取得
+		int year = gcalendar.get(Calendar.YEAR);
+		String sYear = String.valueOf(year);
+		data.setYear(sYear);
+	}
+
+	/**
+	 * 現在の月を取得
+	 */
+	public void getMonth(DataBean data) {
+
+		//現在の月取得
+		int month = gcalendar.get(Calendar.MONTH) + 1;
+		String sMonth = String.valueOf(month);
+		data.setMonth(sMonth);
+	}
+
+	/**
+	 * 年　月　最終日をセット
+	 * 
+	 */
 	public DataBean setData(DataBean data) {
 
 		//現在の年取得
-		GregorianCalendar gcalendar = new GregorianCalendar();
-		int year = gcalendar.get(Calendar.YEAR);
-		String sYear = String.valueOf(year);
-
+		getYear(data);
 		//現在の月取得
-		gcalendar = new GregorianCalendar();
-		int month = gcalendar.get(Calendar.MONTH) + 1;
-		String sMonth = String.valueOf(month);
-
+		getMonth(data);
 		//最終日の取得
-		String sResult = setLastData(data, sYear, sMonth);
-
-		data.setYear(sYear);
-		data.setMonth(sMonth);
-		data.setLastDay(sResult);
+		setLastData(data, data.getYear(), data.getMonth());
 
 		return data;
 	}
+	/**
+	 * 年　月　最終日をセット (2回目用）
+	 * 
+	 */
+	public DataBean setData(DataBean data, String year, String month) {
 
-	public String setLastData(DataBean data, String sYear, String sMonth) {
+		//現在の年取得
+		data.setYear(year);
+		//現在の月取得
+		data.setMonth(month);
+		//最終日の取得
+		setLastData(data, year, month);
+
+		return data;
+	}
+	
+
+	/**
+	 * 最終日の取得
+	 * 
+	 */
+	public void setLastData(DataBean data, String sYear, String sMonth) {
 
 		int year = Integer.valueOf(sYear);
 		int month = Integer.valueOf(sMonth);
@@ -52,53 +89,49 @@ public class KintaiService {
 		int result = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
 		String sResult = String.valueOf(result);
 
-		return sResult;
+		data.setLastDay(sResult);
 	}
 
+	/**
+	 * 表示用データのセット
+	 * 
+	 */
 	public List<DataBean> display(List<DataBean> dataList, DataBean data) {
 
 		String sYear = data.getYear();
 		String sMonth = data.getMonth();
 		String lastDay = data.getLastDay();
-//		List<kintai> kintailist = searchAll();
-//		List<kintai> kintailist = kintaiRepository.findByYmd("2022/04/%");
 
 		for (int i = 1; i <= Integer.valueOf(lastDay); i++) {
 
 			String year = data.getYear();
 			String month = data.getMonth();
 			String day = String.valueOf(i);
-			
+
 			year = String.format("%4s", year).replace(" ", "0");
 			month = String.format("%2s", month).replace(" ", "0");
 			day = String.format("%2s", day).replace(" ", "0");
-			
+
 			String ymd = year + "/" + month + "/" + day;
 			kintai kintailist = kintaiRepository.findByYmd(year + month + day);
-			
+
 			data = new DataBean();
 
 			data.setYear(sYear);
 			data.setMonth(sMonth);
 			data.setYmd(ymd);
-			if(kintailist ==null) {
-				
-			}else {
+			if (kintailist == null) {
+
+			} else {
 				data.setWorkst(kintailist.getWork_st());
 				data.setWorked(kintailist.getWork_ed());
 				data.setWorkrt(kintailist.getWork_rt());
 			}
-			
-		
+
 			dataList.add(data);
 		}
 
 		return dataList;
-	}
-
-	public List<kintai> searchAll() {
-		// ユーザーTBLの内容を全検索
-		return kintaiRepository.findAll();
 	}
 
 	public List<kintai> regist() {
--- kintai-bord/src/main/java/com/example/demo/controlle/KintaiControlle.java
+++ kintai-bord/src/main/java/com/example/demo/controlle/KintaiControlle.java
@@ -18,18 +18,20 @@ public class KintaiControlle {
 	
 	 @Autowired
 	  KintaiService kintaiService;
+	 List<DataBean> dataList = new ArrayList<DataBean>();
 	 
+	 
+	 /*
+	  * 初期表示
+	  * 
+	  */
 	@GetMapping("/form")
 	private String readForm(Model model) {
-
 		
 		DataBean data = new DataBean();
-		List<DataBean> dataList = new ArrayList<DataBean>();
 		
-
 		kintaiService.setData(data);
 		dataList = kintaiService.display(dataList, data);
-		
 		
 		model.addAttribute("data", data);
 		model.addAttribute("dataList", dataList);
@@ -37,28 +39,21 @@ public class KintaiControlle {
 		return "index";
 	}
 
-	//	@GetMapping("/form")
-	//	private String readForm(Model model) {
-	//			
-	//		DataBean data = new DataBean();
-	//		KintaiService.setData(data);
-	//		
-	//		model.addAttribute("data", data);
-	//		
-	//		return "index";
-	//	}
-
+	/*
+	 * 2回目以降の表示
+	 * 
+	 */
 	@PostMapping("/form")
 	private String Form(@RequestParam("year") String year, @RequestParam("month") String month, DataBean data,
-			Model model) {
+			@RequestParam("work_st") String workst,@RequestParam("work_ed") String worked,@RequestParam("work_rt") String workrt, Model model) {
 
-		data.setYear(year);
-		data.setMonth(month);
-		data.setLastDay(kintaiService.setLastData(data, year, month));
-
-		List<DataBean> dataList = new ArrayList<DataBean>();
-
+		dataList = new ArrayList<DataBean>();
+		
+		data = kintaiService.setData(data , year , month);
 		dataList = kintaiService.display(dataList, data);
+		String[] work_st = workst.split(",", -1);
+		String[] work_ed = worked.split(",", -1);
+		String[] work_rt = workrt.split(",", -1);
 
 		model.addAttribute("data", data);
 		model.addAttribute("dataList", dataList);
